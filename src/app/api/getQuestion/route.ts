05f4f433e7e39f86e01648f5315eaf1d1012f884
Dev <devharwani.work@gmail.com>
2025-07-26 14:40:23 +0000

    update response to give out necessary details only

--- src/app/api/getQuestion/route.ts
+++ src/app/api/getQuestion/route.ts
@@ -22,15 +22,83 @@ export const POST = async (req: NextRequest) => {
                 slug
             },
             include: {
-                answers: true,
+                answers: {
+                    include: {
+                        votes: true
+                    }
+                },
                 votes: true,
                 tags: true,
                 notifications: true
             }
         });
-        return NextResponse.json(question, { status: 201 });
+        const tagsInQuestion: string[] = [];
+        await Promise.all(
+            question?.tags.map(async (tag) => {
+                const response = await db.tag.findFirst({
+                    where: { id: tag.tagId }
+                });
+                if (response) {
+                    tagsInQuestion.push(response.name);
+                }
+            }) ?? []
+        );
+        const author = await db.user.findFirst({
+            where: {
+                id: question?.authorId
+            }
+        });
+        let answers: {
+            createdAt: string;
+            content: string;
+            authorName: string;
+            votes: number;
+        }[] = []
+
+        
+        await Promise.all(
+            question?.answers.map(async (answer) => {
+                const author = await db.user.findUnique({
+                    where: { id: answer.authorId }
+                })
+                answers.push({
+                    createdAt: new Date(answer.createdAt).toLocaleDateString(),
+                    content: answer.content,
+                    authorName: author?.name ?? "",
+                    votes: answer.votes.length
+                })
+            }) ?? []
+        )
+        question?.answers.map((answer) => {
+
+        })
+
+        const name = author?.name;
+        const isoString = question?.createdAt;
+        const date = new Date(isoString!).toLocaleDateString();
+        const response = {
+            title: question?.title,
+            description: question?.description,
+            votesLength: question?.votes.length,
+            createdAt: date,
+            tagsInQuestion,
+            answers,
+            name
+        }
+        // setQuestionContent(response.data.question.description);
+        // setVotesCount(response.data.question.votes.length);
+        // setQuestionTitle(response.data.question.title);
+        // const isoString = response.data.question.createdAt;
+        // const date = new Date(isoString);
+        // setCreatedAt(date.toLocaleDateString());
+        // setQuestionTags(response.data.tagsInQuestion);
+        // setAnswers(response.data.question.answers);
+        // setAnswerVotesLength(response.data.question.answers.length)
+        // setQuestionAuthor(response.data.name);
+
+        return NextResponse.json(response, { status: 201 });
     } catch (error) {
-        return NextResponse.json({error: "Failed to fetch the question"}, {status: 500})
+        return NextResponse.json({ error: "Failed to fetch the question" }, { status: 500 })
     }
 
 
