afa82c81b9f13dd882848a68d39f1cb60d24a257
Dev <devharwani.work@gmail.com>
2025-08-05 15:20:51 +0000

    add getNotifications route

---
+++ src/app/api/getNotifications/route.ts
@@ -0,0 +1,35 @@
+import { auth } from "@/server/auth";
+import { db } from "@/server/db";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    const session = await auth();
+    if (!session?.user?.id) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        const notifications = await db.notification.findMany({
+            where: {
+                userId: session.user.id
+            },
+            include: {
+                question: {
+                    select: {
+                        slug: true
+                    }
+                }
+            }
+        });
+
+        const response = notifications.map((notification) => ({
+            message: notification.message,
+            createdAt: new Date(notification.createdAt).toLocaleDateString("en-IN"),
+            slug: notification.question?.slug
+        }));
+
+        return NextResponse.json(response, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: "Error fetching all notifications." }, { status: 500 });
+    }
+}
